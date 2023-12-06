d2553f5c448839162676e27e0466c5932626e2b9
g-ka <gkaphilathithiya@gmail.com>
2023-12-06 23:48:44 +0530

    update_basename

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Data_provider } from './context/Data_box';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename='/chatbox'>
       <Data_provider>
         <Routes>
           <Route path='/*' element={<App />}/>
